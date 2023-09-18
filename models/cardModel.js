29145583848f085f36873ceb6c8652740f394f14
amoscoloni <78222489+amoscoloni@users.noreply.github.com>
2023-09-18 13:14:14 -0400

    removed dead code

--- models/cardModel.js
+++ models/cardModel.js
@@ -1,7 +1,6 @@
 const mysql = require( 'mysql2/promise' );
 const validator = require( './validateCard' );
 const logger = require( '../logger' );
-// const database = require( './databaseModel' );
 var connection;
 
 //#region ERROR_CLASSES
